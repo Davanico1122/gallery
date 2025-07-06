52ec2e7262e097b1716bb6e4599f33ccef9e3e4b
Davanico <davaniko40@gmail.com>
2025-07-06 11:21:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const boxes = document.querySelectorAll('.image-box');
-
+const imageBoxes = document.querySelectorAll('.image-box');
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
     }
   });
-}, {
-  threshold: 0.3
-});
-
-boxes.forEach(box => observer.observe(box));
+}, { threshold: 0.25 });
+imageBoxes.forEach(box => observer.observe(box));
